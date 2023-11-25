d47548a25fe1983dd7ec307e60086266f88195e2
YoruH03 <yuri03ysl@gmail.com>
2023-11-25 19:50:24 -0300

    adição de módulo de menu de busca

---
+++ menu_de_busca.py
@@ -0,0 +1,17 @@
+import PySimpleGUI as sg
+def CRIAR_MENU():    
+    sg.theme('dark blue 14')
+    logo_path = "E:\@Meus Documentos\Downloads\logo_fundo.png"
+    menu_layout = [[sg.Image(filename=logo_path, enable_events=True)],
+                [sg.Text('O que vamos comer hoje?',font=('Arial',30,'bold'),justification='center')],
+                [sg.Input(key='-INPUT-')],
+                [sg.Button('Buscar'),sg.Button('Não quero comer nada')]]
+
+    menu_principal = sg.Window('Food Finder',menu_layout,finalize=True,element_justification='center')
+
+    while True:
+        event,values = menu_principal.read()
+        if event==sg.WINDOW_CLOSED or event=='Não quero comer nada':
+            break
+
+    menu_principal.close()
