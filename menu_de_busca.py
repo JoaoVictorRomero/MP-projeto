ac9f6b1991da10d0724961935b9d34d8ea43164a
YoruH03 <yuri03ysl@gmail.com>
2023-11-26 13:43:26 -0300

    Mudanças de layout da pagina inicial, separação por botões cada janela

--- menu_de_busca.py
+++ menu_de_busca.py
@@ -1,34 +1,44 @@
 """Framework que permite a criação de GUI nos python."""
 import PySimpleGUI as sg
+sg.theme('dark blue 14')
+logo_path = r"assets\logo_fundo.png"
 def criar_menu():
     """Função que leva a criação do menu de busca"""
-    sg.theme('dark blue 14')
-    logo_path = r"assets\logo_fundo.png"
-    menu_layout = [[sg.Image(filename=logo_path, enable_events=True)],
-                [sg.Text('O que vamos comer hoje?',font=('Arial',30,'bold'),
+    menu_layout = [
+                [sg.Button(key="-SAIR-",button_color=("#cd2323",sg.theme_background_color),size=(3,3),button_text="Sair",font=("Arial",30,'bold'),border_width=0),
+                sg.Button(button_color=('#00d027', sg.theme_background_color), border_width=0,size=(10,3),button_text="Buscar",font=("Arial",35), key='-BUSCAR-'),\
+                sg.Text('|',font=("Arial",30,'bold')), sg.Button(button_color=('white', sg.theme_background_color), border_width=0,size=(10,3),button_text="Prato do dia",font=("Arial",35), key='-PRATO_DO_DIA-'),\
+                sg.Text('|',font=("Arial",30,'bold')), sg.Button(button_color=('white', sg.theme_background_color), border_width=0,size=(10,3),button_text="Mapa",font=("Arial",35), key='-MAPA-'),sg.Text(''),
+                sg.Text('|',font=("Arial",30,'bold')), sg.Button(button_color=('white', sg.theme_background_color), border_width=0,size=(10,3),button_text="Login",font=("Arial",30), key='-LOGIN-')],
+                [sg.Image(filename=logo_path, enable_events=True)],
+                [sg.Text('Qual sua fome hoje?',font=('Arial',30,'bold'),
                          justification='center')],
                 [sg.Input(key='-INPUT-')],
-                [sg.Button('Buscar'),sg.Button('Não quero comer nada')]]
+                [sg.Button(button_text="Buscar",font=("Arial",30,'bold'),button_color=('#23cd4b', sg.theme_background_color),border_width=0),
+                 sg.Button(button_text="Não quero comer nada",font=("Arial",20,'bold'),button_color=('#cd2323', sg.theme_background_color),border_width=0)]]
 
-    menu_principal = sg.Window('Food Finder',menu_layout,finalize=True,
+    menu_busca = sg.Window('Food Finder',menu_layout,finalize=True,
                                element_justification='center')
+    menu_busca.maximize()
 
     while True:
-        event = menu_principal.read()
-        if event==sg.WINDOW_CLOSED or event=='Não quero comer nada':
+        event,values = menu_busca.read()
+        if event==sg.WINDOW_CLOSED or event=='Não quero comer nada' or event=="-SAIR-":
             break
+        if event=="-SAIR-":
+            for window in sg.window_list():
+                sg.Window(window).close()
 
-    menu_principal.close()
+    menu_busca.close()
 
 def test_criar_menu():
     """Função que testa a criação do menu inicial"""
-    sg.theme('dark blue 14')
-    logo_path = r"assets\logo_fundo.png"
     menu_layout = [[sg.Image(filename=logo_path, enable_events=True)],
                 [sg.Text('O que vamos comer hoje?',font=('Arial',30,'bold'),
                          justification='center')],
                 [sg.Input(key='-INPUT-')],
-                [sg.Button('Buscar'),sg.Button('Não quero comer nada')]]
+                [sg.Button(button_text="Buscar",font=("Arial",30,'bold'),button_color=('#23cd4b', sg.theme_background_color),border_width=0),
+                 sg.Button(button_text="Não quero comer nada",font=("Arial",30,'bold'),button_color=('#cd2323', sg.theme_background_color),border_width=0)]]
 
     menu_principal = sg.Window('Food Finder',menu_layout,finalize=True,
                                element_justification='center')
--- tela_inicial.py
+++ tela_inicial.py
@@ -1,21 +1,31 @@
 import PySimpleGUI as sg
-from menu_de_busca import CRIAR_MENU
+from menu_de_busca import criar_menu
+from menu_de_busca import logo_path
 
 background_image = r"assets\background_4.png"
+logo_big_path = r"assets\logo_big.png"
 
 # Set the theme
-sg.theme('Dark Blue 14')
+sg.theme_background_color="#21273d"
 
 # Create the main layout
 main_layout = [
-    [sg.Button("Sair",key="-SAIR-")],
-    [sg.Image(filename=background_image,size=(6000,1000))],
+    [sg.Button(key="-SAIR-",button_color=("#cd2323",sg.theme_background_color),size=(3,3),button_text="Sair",font=("Arial",30,'bold'),border_width=0),
+    sg.Button(button_color=('white', sg.theme_background_color), border_width=0,size=(10,3),button_text="Buscar",font=("Arial",35), key='-BUSCAR-'),\
+    sg.Text('|',font=("Arial",30,'bold')), sg.Button(button_color=('white', sg.theme_background_color), border_width=0,size=(10,3),button_text="Prato do dia",font=("Arial",35), key='-PRATO_DO_DIA-'),\
+    sg.Text('|',font=("Arial",30,'bold')), sg.Button(button_color=('white', sg.theme_background_color), border_width=0,size=(10,3),button_text="Mapa",font=("Arial",35), key='-MAPA-'),sg.Text(''),
+    sg.Text('|',font=("Arial",30,'bold')), sg.Button(button_color=('white', sg.theme_background_color), border_width=0,size=(10,3),button_text="Login",font=("Arial",30), key='-LOGIN-')],
+    [sg.Text('')],
+    [sg.Text('')],
+    [sg.Text('')],
+    [sg.Text('')],
+    [sg.Image(filename=logo_path)],
+    [sg.Text("Food Finder",font=("Arial",60,"bold"))]
 ]
 
 # Create the main window
-main = sg.Window("Food Finder", main_layout,finalize=True)
+main = sg.Window("Food Finder", main_layout,element_justification='center',finalize=True)
 main.Maximize()
-CRIAR_MENU()
 
 # Event loop
 while True:
@@ -24,6 +34,8 @@ while True:
     # Check if the window was closed
     if event == sg.WINDOW_CLOSED or event =="-SAIR-":
         break
+    if event == "-BUSCAR-":
+        criar_menu()
 
 # Close the window
 main.close()
