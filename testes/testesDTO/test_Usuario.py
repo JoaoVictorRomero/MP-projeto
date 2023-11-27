5d888932bac2c41b85594bc52183c33f83a9b941
Marcelo Vitor <marcelovitor890@gmail.com>
2023-11-27 09:50:23 -0300

    Testes DTO.Usuario

--- testes/testesDTO/test_Usuario.py
+++ testes/testesDTO/test_Usuario.py
@@ -1,10 +1,23 @@
 from DTO.Usuario import Usuario
 
-def test_criar_usuario_sucesso():
-    novo_usuario = Usuario(id_usuario=1, nome_usuario='Teste', funcao='Teste', login='teste@gmail.com', senha='senha')
+# Teste para verificar se a inicialização do usuário está funcionando corretamente
+def test_criacao_usuario():
+    usuario = Usuario(1, 'João', 'Admin', 'joao@example.com', 'senha123')
+    assert usuario.getId_usuario() == 1
+    assert usuario.getNome_usuario() == 'João'
+    assert usuario.getFuncao() == 'Admin'
+    assert usuario.getLogin() == 'joao@example.com'
+    assert usuario.getSenha() == 'senha123'
 
-    assert novo_usuario.getId_usuario() == 1
-    assert novo_usuario.getNome_usuario() == 'Teste'
-    assert novo_usuario.getFuncao() == 'Teste'
-    assert novo_usuario.getLogin() == 'teste@gmail.com'
-    assert novo_usuario.getSenha() == 'senha'
+# Teste para verificar se a atualização do usuário está funcionando corretamente
+def test_atualizacao_usuario():
+    usuario = Usuario(1, 'João', 'Admin', 'joao@example.com', 'senha123')
+    usuario.setNome_usuario('José')
+    usuario.setFuncao('Supervisor')
+    usuario.setLogin('jose@example.com')
+    usuario.setSenha('novasenha456')
+    
+    assert usuario.getNome_usuario() == 'José'
+    assert usuario.getFuncao() == 'Supervisor'
+    assert usuario.getLogin() == 'jose@example.com'
+    assert usuario.getSenha() == 'novasenha456'
