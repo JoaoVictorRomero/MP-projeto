4a2956ae99ce0f07cbaf014c05b50079b24b7428
Marcelo Vitor <marcelovitor890@gmail.com>
2023-11-27 08:27:54 -0300

    Teste DTO.Usuario

--- testes/testesDTO/test_Usuario.py
+++ testes/testesDTO/test_Usuario.py
@@ -3,4 +3,8 @@ from DTO.Usuario import Usuario
 def test_criar_usuario():
     novo_usuario = Usuario(id_usuario=1, nome_usuario='Teste', funcao='Teste', login='teste@gmail.com', senha='senha')
 
+    assert novo_usuario.getId_usuario() == 1
     assert novo_usuario.getNome_usuario() == 'Teste'
+    assert novo_usuario.getFuncao() == 'Teste'
+    assert novo_usuario.getLogin() == 'teste@gmail.com'
+    assert novo_usuario.getSenha() == 'senha'
