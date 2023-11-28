2130234e90be5252dff9ea3b4062ccbc48e92315
MarceloVitor01 <marcelovitor890@gmail.com>
2023-11-28 19:40:05 -0300

    Atualização testes

--- test/testesDTO/test_pesquisa_restaurante.py
+++ test/testesDTO/test_pesquisa_restaurante.py
@@ -1,14 +1,14 @@
 from Pesquisa_Restaurante import Pesquisa_Restaurante
 
-#Teste para verificar a inicialização da pesquisa por restaurante
 def test_inicializacao_pesquisa_restaurante():
+    '''Teste para verificar a inicialização da pesquisa por restaurante'''
     pesquisa_restaurante = Pesquisa_Restaurante(1, 1, 1)
     assert pesquisa_restaurante.get_id_pesquisa_restaurante() == 1
     assert pesquisa_restaurante.get_fk_id_usuario() == 1
     assert pesquisa_restaurante.get_fk_id_restaurante() == 1
 
-#Teste para verificar a atualização da pesquisa por restaurante
 def test_atualizacao_pesquisa_restaurante():
+    '''Teste para verificar a atualização da pesquisa por restaurante'''
     pesquisa_restaurante = Pesquisa_Restaurante(1, 1, 1)
 
     pesquisa_restaurante.set_id_pesquisa_restaurante(2)
--- test/testesDTO/test_produto.py
+++ test/testesDTO/test_produto.py
@@ -1,14 +1,14 @@
 from DTO.Produto import Produto
 
-# Teste para verificar a inicialização do produto
 def test_inicializacao_produto():
+    '''Teste para verificar a inicialização do produto'''
     produto = Produto(1, 'Produto A', 1)
     assert produto.get_id_produto() == 1
     assert produto.get_nome_produto() == 'Produto A'
     assert produto.get_fk_id_restaurante() == 1
 
-# Teste para verificar a atualização dos atributos do produto
 def test_atualizacao_produto():
+    '''Teste para verificar a atualização dos atributos do produto'''
     produto = Produto(1, 'Produto A', 1)
     
     produto.set_nome_produto('Novo Produto')
