f7e74a1d6fa18e05dca0823ef2f2d8c4b25ba49f
MarceloVitor01 <marcelovitor890@gmail.com>
2023-11-28 19:38:21 -0300

    Teste pesquisa_restaurante

--- test/testesDTO/test_pesquisa_restaurante.py
+++ test/testesDTO/test_pesquisa_restaurante.py
@@ -1,7 +1,19 @@
 from Pesquisa_Restaurante import Pesquisa_Restaurante
 
-def test_criacao_pesquisa_restaurante():
+#Teste para verificar a inicialização da pesquisa por restaurante
+def test_inicializacao_pesquisa_restaurante():
     pesquisa_restaurante = Pesquisa_Restaurante(1, 1, 1)
-    assert pesquisa_restaurante.get_id_pesquisa_restaurante == 1
-    assert pesquisa_restaurante.get_fk_id_usuario== 1
-    assert pesquisa_restaurante.get_fk_id_restaurante == 1
+    assert pesquisa_restaurante.get_id_pesquisa_restaurante() == 1
+    assert pesquisa_restaurante.get_fk_id_usuario() == 1
+    assert pesquisa_restaurante.get_fk_id_restaurante() == 1
+
+#Teste para verificar a atualização da pesquisa por restaurante
+def test_atualizacao_pesquisa_restaurante():
+    pesquisa_restaurante = Pesquisa_Restaurante(1, 1, 1)
+
+    pesquisa_restaurante.set_id_pesquisa_restaurante(2)
+    pesquisa_restaurante.set_fk_id_usuario(2)
+
+    assert pesquisa_restaurante.get_id_pesquisa_restaurante() == 2
+    assert pesquisa_restaurante.get_fk_id_usuario() == 2
+    assert pesquisa_restaurante.get_fk_id_restaurante() == 1
