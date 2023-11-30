eae3fd768095d84c478d074ab9f063326dc06fe2
Marcelo Vitor <marcelovitor890@gmail.com>
2023-11-29 22:29:55 -0300

    Atualização

--- teste_import.py
+++ teste_import.py
@@ -1,4 +1,8 @@
-from backend.Classe_Usuario import Usuario
+import sys
+sys.path.append('../backend')
+
+from DTO.classe_usuario import Usuario
+
 usuario_novo = Usuario(1, 'Teste', 'Função', 'login', 'senha')
 
 print(usuario_novo)
